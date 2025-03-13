5e0fdf6e1add7175ef6de9edb5c8acebd07bc16b
Rodion <rodionchik17@gmail.com>
2025-03-13 20:42:07 +0200

    created interface UserRepository

---
+++ src/main/java/microservice/module/securityjwtauth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package microservice.module.securityjwtauth.repository;
+
+import microservice.module.securityjwtauth.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
